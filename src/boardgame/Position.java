88ddc6949fa5ad640a634ea1115fcc275fdd3c8c
hmarinoo <hmarinoo@gmail.com>
2019-02-01 23:03:59 -0200

    First class: Postion

---
+++ src/boardgame/Position.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Position {
+
+	private Integer rowl;
+	private Integer column;
+	public Position(Integer rowl, Integer column) {
+		super();
+		this.rowl = rowl;
+		this.column = column;
+	}
+	public Integer getRowl() {
+		return rowl;
+	}
+	public void setRowl(Integer rowl) {
+		this.rowl = rowl;
+	}
+	public Integer getColumn() {
+		return column;
+	}
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return rowl + ", "+ column; 
+	}
+	
+}
