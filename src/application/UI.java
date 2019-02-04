6e0e0c328c05fdf8918764e43844d88ae359249d
hmarinoo <hmarinoo@gmail.com>
2019-02-04 15:06:54 -0200

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,18 +32,24 @@ public class UI {
 
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		Character column = s.charAt(0);
-		Integer row = Integer.parseInt(s.substring(1));
-		
-		return new ChessPosition(column, row);
-		}
-		catch(RuntimeException e) {
+			String s = sc.nextLine();
+			Character column = s.charAt(0);
+			Integer row = Integer.parseInt(s.substring(1));
+
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid value are from a1 to h8");
 		}
 	}
-	
+
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static void printBoard(ChessPiece[][] pieces) {
+		clearScreen();
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
